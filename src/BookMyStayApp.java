9af87b1f313b532d77e7424ac44e925ec488bc6b
tadepalliruthvika <tadepalliruthvika@gmail.com>
2026-03-18 10:03:41 +0530

    UC3

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 abstract class Room {
     String type;
     int beds;
@@ -34,6 +36,31 @@ class SuiteRoom extends Room {
     }
 }
 
+class RoomInventory {
+    private HashMap<String, Integer> inventory;
+
+    RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 2);
+    }
+
+    int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
+    }
+
+    void updateAvailability(String roomType, int count) {
+        inventory.put(roomType, count);
+    }
+
+    void displayInventory() {
+        for (String key : inventory.keySet()) {
+            System.out.println(key + " Available: " + inventory.get(key));
+        }
+    }
+}
+
 public class BookMyStayApp {
 
     public static void main(String[] args) {
@@ -42,24 +69,25 @@ public class BookMyStayApp {
         Room doubleRoom = new DoubleRoom();
         Room suite = new SuiteRoom();
 
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
+        RoomInventory inventory = new RoomInventory();
 
         System.out.println("===== Book My Stay App =====");
-        System.out.println("Version: 2.1");
+        System.out.println("Version: 3.1");
         System.out.println("============================");
 
         single.displayDetails();
-        System.out.println("Available: " + singleAvailable);
+        System.out.println("Available: " + inventory.getAvailability("Single Room"));
         System.out.println("----------------------------");
 
         doubleRoom.displayDetails();
-        System.out.println("Available: " + doubleAvailable);
+        System.out.println("Available: " + inventory.getAvailability("Double Room"));
         System.out.println("----------------------------");
 
         suite.displayDetails();
-        System.out.println("Available: " + suiteAvailable);
+        System.out.println("Available: " + inventory.getAvailability("Suite Room"));
         System.out.println("----------------------------");
+
+        System.out.println("===== Current Inventory =====");
+        inventory.displayInventory();
     }
 }
